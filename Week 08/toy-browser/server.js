aa692733bc2ead18005c9cb061d7e2009bddc1ec
jiangjj <jiangjj@sse.net.cn>
2020-12-04 15:13:53 +0800

    20201204-office-week08

--- Week 08/toy-browser/server.js
+++ Week 08/toy-browser/server.js
@@ -11,15 +11,25 @@ http.createServer((request,response)=>{
     body.push(chunk.toString())//toString()为什么？？chunk原来是什么类型？？
   })
   .on('end',()=>{
-    body = Buffer.concat(body).toString()//为什么用Buffer concat
+    body = body.join('')
     console.log('body:',body)
 
     response.writeHead(200,{'Content-Type':'text/html'})
-    response.end(' hello\n')
+    response.end(' hello'.repeat(500))
   })
 }).listen(8088)
 
 console.log('server started');
+
+
+
+
+
+
+
+
+
+
 
 // let buf1 = new Buffer('宝宝');
 // let buf2 = new BUffer('baobao');
